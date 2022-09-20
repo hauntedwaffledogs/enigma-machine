c43711211076f8b8b5bd1f67710effbc7f1bce6a
hauntedwaffledogs <70719506+hauntedwaffledogs@users.noreply.github.com>
2022-09-20 14:00:51 -0500

    added some comments

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -3,5 +3,9 @@ import numpy as np
 #Picks a random number between 97 and 122
 x = np.random.randint(97, 122)
 
+
+#prints the ascii number
 print (x)
+
+#shows the translated ascii char
 print(chr(x))
