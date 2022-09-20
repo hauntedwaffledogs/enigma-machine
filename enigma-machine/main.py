ee3d951746bd0dd462eaac7aa7e76208edb91b92
hauntedwaffledogs <evan1evan123@gmail.com>
2022-09-20 10:49:57 -0500

    reverted some changes

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -2,17 +2,15 @@ import numpy as np
 import string
 
 
-print("type your message")
-inputs = input()
 
 #this makes the alphabet
 alphabet = np.array(list(string.ascii_lowercase + ' '))
 
 #Picks a random number between 97 and 122
-x = np.random.randint(24, size=64)
+x = np.random.randint(23, size=64)
 
 
 #output
 output = np.random.choice(alphabet, size=len(x))
 
-print(inputs, output)
+print(output)
