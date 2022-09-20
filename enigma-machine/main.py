00787636c98622ff6f7f5b2e645fd239908d2099
hauntedwaffledogs <evan1evan123@gmail.com>
2022-09-20 10:25:17 -0500

    encryption working

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -6,5 +6,4 @@ alphabet = np.array(list(string.ascii_lowercase + ' '))
 #Picks a random number between 97 and 122
 x = np.random.randint(97, 122, size=64)
 
-print(x)
-print(alphabet)
+print(np.random.choice(alphabet, size=len(x)))
