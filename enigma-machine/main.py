a7e1a0da6aecf514c21183137d1c86f25f52c8bf
hauntedwaffledogs <evan1evan123@gmail.com>
2022-09-20 10:28:08 -0500

    added comments

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -1,9 +1,12 @@
 import numpy as np
 import string
 
+#this makes the alphabet
 alphabet = np.array(list(string.ascii_lowercase + ' '))
 
 #Picks a random number between 97 and 122
 x = np.random.randint(97, 122, size=64)
 
+
+#output
 print(np.random.choice(alphabet, size=len(x)))
