16623068c489821824490a917536d6eac38190db
hauntedwaffledogs <evan1evan123@gmail.com>
2022-09-21 04:49:24 -0500

    simplified encoder

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -1,21 +1,10 @@
 import numpy as np
 import string
 
-
-
-#this makes the alphabet
-alphabet = np.array(list(string.ascii_lowercase + ' '))
-
 #Picks a random number between 97 and 122
 x = np.random.randint(24, size=64)
-RandomNumber = np.random.randint(111111, 999999)
-seed = np.random.SeedSequence(RandomNumber)
-
 
 #output
-encoder = np.random.choice(alphabet, size=len(x))
-
-
-
+encoder = np.random.choice(x, size=len(x))
 
 print(encoder)
