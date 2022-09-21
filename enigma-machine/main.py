61a52cd9ae66e1649448c9c9ed4bba60800291e0
hauntedwaffledogs <evan1evan123@gmail.com>
2022-09-21 05:18:01 -0500

    rewrote encoder

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -1,10 +1,5 @@
 import numpy as np
 import string
 
-#Picks a random number between 97 and 122
-x = np.random.randint(24, size=64)
-
-#output
-encoder = np.random.choice(x, size=len(x))
-
-print(encoder)
+b = np.random.randint(24, size=64)
+print(b)
