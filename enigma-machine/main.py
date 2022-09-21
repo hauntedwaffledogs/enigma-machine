e99979cb2aec8260950d17cdf227af0fa93a210a
hauntedwaffledogs <evan1evan123@gmail.com>
2022-09-21 06:39:18 -0500

    rewrote the encoder again

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -1,14 +1,8 @@
 import numpy as np
 import string
 
-alp = string.ascii_lowercase
+alp = list(string.ascii_lowercase)
 
-encoder1 = np.random.randint(97, 123, size=64)
-encoder2 = np.random.randint(97, 123, size=64)
-encoder3 = np.random.randint(97, 123, size=64)
+encoder1 = np.random.choice(alp, size=64)
 
-
-
-
-
-print(encoder1, encoder2, encoder3)
+print(encoder1)
