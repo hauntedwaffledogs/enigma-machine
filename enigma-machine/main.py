65f1109f147adcaa13f3f9aa8329a7b7056dd522
hauntedwaffledogs <evan1evan123@gmail.com>
2022-09-22 05:36:09 -0500

    j

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -3,10 +3,10 @@ import string
 
 alp = list(string.ascii_lowercase)
 
-seed = 64876
-
 encoder1 = np.random.choice(alp, size=24)
+encoder2 = np.random.choice(alp, size=24)
+encoder3 = np.random.choice(alp, size=24)
 
-print(encoder1)
+print(encoder1, encoder2, encoder3)
 
 
