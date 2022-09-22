1cc200b7ee60eab9ff7bf52f07b86b3caf61bd4d
hauntedwaffledogs <evan1evan123@gmail.com>
2022-09-22 05:41:59 -0500

    zzzzzz

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -8,14 +8,3 @@ encoder2 = np.random.choice(alp, size=24)
 encoder3 = np.random.choice(alp, size=24)
 
 print(encoder1, encoder2, encoder3)
-
-<<<<<<< HEAD
-#Picks a random number between 97 and 122
-x = np.random.randint(97, 122, size=64)
-
-
-
-print(x)
-=======
-
->>>>>>> origin/testing
