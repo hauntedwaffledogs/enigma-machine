7d0bb69fb6d2f0d38e1521f28dbff25e64f20d65
hauntedwaffledogs <evan1evan123@gmail.com>
2022-09-26 05:23:41 -0500

    working roters

--- enigma-machine/seed.py
+++ enigma-machine/seed.py
@@ -1,10 +1,14 @@
 import numpy as np
 import string
 
-seed = 2
+seed1 = 2
+seed2 = 3
+seed3 = 4
+alphabet1 = list(map(chr, range(97+seed1, 123-seed1)))
+alphabet2 = list(map(chr, range(97+seed2, 123-seed2)))
+alphabet3 = list(map(chr, range(97+seed3, 123-seed3)))
 
-alphabet = list(map(chr, range(97+seed, 123-seed)))
 
-output = alphabet
 
-print(output)
+
+print(alphabet1, alphabet2, alphabet3)
