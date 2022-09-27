0f82d9ae24f13d9e0a6e9b781b36c5c1da467e3f
hauntedwaffledogs <evan1evan123@gmail.com>
2022-09-27 06:55:42 -0700

    removed seed.py

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -2,11 +2,6 @@ import numpy as np
 import string
 
 
-alp = list(string.ascii_lowercase)
+x = list(map(chr, range(ord('a'), ord('z')+1)))
 
-encoder1 = np.random.choice(alp, size=24)
-encoder2 = np.random.choice(alp, size=24)
-encoder3 = np.random.choice(alp, size=24)
-
-print(encoder1, encoder2, encoder3)
-
+print(x)
--- enigma-machine/seed.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import string
-
-
-x = list(map(chr, range(ord('a'), ord('z')+1)))
-
-print(x)
