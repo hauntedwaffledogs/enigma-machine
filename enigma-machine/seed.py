c61dc80f552acfa1fade81c89de5c1e630226028
hauntedwaffledogs <evan1evan123@gmail.com>
2022-09-27 01:49:38 -0500

    l

--- enigma-machine/seed.py
+++ enigma-machine/seed.py
@@ -1,16 +1,7 @@
 import numpy as np
 import string
 
-<<<<<<< Updated upstream
-seed1 = 2
-seed2 = 3
-seed3 = 4
-alphabet1 = list(map(chr, range(97+seed1, 123-seed1)))
-alphabet2 = list(map(chr, range(97+seed2, 123-seed2)))
-alphabet3 = list(map(chr, range(97+seed3, 123-seed3)))
 
+x = list(map(chr, range(ord('a'), ord('z')+1)))
 
-
-=======
->>>>>>> Stashed changes
-
+print(x)
