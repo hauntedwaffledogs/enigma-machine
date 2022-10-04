9eea84d3927cbf0031b95e1a433ca14de223ae16
hauntedwaffledogs <evan1evan123@gmail.com>
2022-10-04 06:50:55 -0700

    j

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -1,7 +1,5 @@
 import numpy as np
 import string
 
-roter1 = list(map(chr, range(97, 123)))
-roter2 = list(map(chr, range(97, 123)))
-roter3 = list(map(chr, range(97, 123)))
-print(roter1)
+
+print()
