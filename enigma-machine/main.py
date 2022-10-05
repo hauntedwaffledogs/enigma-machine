6ee49d0e10624ae7eb4500bbae70013cbed85d25
hauntedwaffledogs <evan1evan123@gmail.com>
2022-10-05 09:39:06 -0500

    main is up to date

---
+++ enigma-machine/main.py
@@ -0,0 +1,23 @@
+import numpy as np
+import string
+
+plugboard1 = 97+5
+plugboard2 = 97+6
+plugboard3 = 97+4
+plugboard4 = 97+10
+plugboard5 = 97+2
+plugboard6 = 97+15
+
+router1 = 97 +12
+router2 = 97 +2
+router3 = 97 +4
+router4 = 97 +6
+router5 = 97 +8
+router6 = 97 +10
+router7 = 97 +11
+router8 = 97 +13
+router9 = 97 +1
+router10 = 97 +0
+
+print(chr(plugboard1), chr(plugboard2), chr(plugboard3), chr(plugboard4), chr(plugboard5), chr(plugboard6))
+print(chr(router1), chr(router2), chr(router3), chr(router4), chr(router5), chr(router6), chr(router7), chr(router8), chr(router9), chr(router10))
