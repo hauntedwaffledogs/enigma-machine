acd0c9dcbc43558a79531a246da16143517540d8
hauntedwaffledogs <evan1evan123@gmail.com>
2022-10-05 06:46:02 -0700

    PLUGBOARD!!!

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -1,13 +1,11 @@
-from re import X
 import numpy as np
 import string
 
-x = "s"
+plugboard1 = 97+5
+plugboard2 = 97+6
+plugboard3 = 97+4
+plugboard4 = 97+10
+plugboard5 = 97+2
+plugboard6 = 97+15
 
-rotor1 = ord(x)+3
-rotor2 = rotor1+2
-rotor3 = rotor2-10
-
-
-
-print(chr(rotor3))
+print(chr(plugboard1), chr(plugboard2), chr(plugboard3), chr(plugboard4), chr(plugboard5), chr(plugboard6))
