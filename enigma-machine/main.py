0bbe7afef93a4df9d4fbd822d279ba0b3672acac
hauntedwaffledogs <evan1evan123@gmail.com>
2022-10-10 16:58:25 -0500

    seed

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -1,12 +1,13 @@
 import numpy as np
 import string
 #inputs = input()
-
+seed = 64254
 
 def rotor(rotor):
     alphabet = list(string.ascii_lowercase)
     rotor1 = np.random.choice(alphabet, size=26)
-    print(rotor1)
+    a = str(rotor1)
+    print(a)
 
 rotor(rotor)
 
