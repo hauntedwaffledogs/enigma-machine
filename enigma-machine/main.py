2bb28ab42e5448f1afdc0dec604bb752a2450db5
hauntedwaffledogs <evan1evan123@gmail.com>
2022-10-10 16:36:04 -0500

    j

---
+++ enigma-machine/main.py
@@ -0,0 +1,13 @@
+import numpy as np
+from tempfile import TemporaryFile
+#inputs = input()
+
+
+def rotor(rotor):
+    rotor1 = np.random.randint(26, size=67)
+    a = rotor1 + 97
+    x = list(a)
+    print(x)
+
+rotor(rotor)
+
