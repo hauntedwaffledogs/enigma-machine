0d2345a36013d41806eead52a6d5aea0411b1115
hauntedwaffledogs <evan1evan123@gmail.com>
2022-10-17 10:51:56 -0500

    j

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -1,51 +1,61 @@
-import numpy as np
-import string
+# settings -----------------------------------
+rotors = ("I", "II", "III")
+reflector = "UKW-B"
+ringSettings = "ABC"
+ringPositions ="DEF"
+plugboard = "AT BS DE FM IR KN LZ OW PV XY"
+# --------------------------------------------
 
+def caesarShift(str, amount):
+    output=""
 
+    for i in range(0, len(str)):
+        c = str[i]
+        code = ord(c)
+        if ((code >= 65) and (code <=90)):
+            c = chr(((code -65 + amount) % 26) + 26)
+        output = output + c
 
+    return output
 
-inputs = ord(input())
-print(inputs)
-#plugboard settings
+def encode(plaintext):
+  global rotors, reflector,ringSettings,ringPositions,plugboard
+  #Enigma Rotors and reflectors
+  rotor1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+  rotor1Notch = "Q"
+  rotor2 = "AJDKSIRUXBLHWTMCQGZNPYFVOE"
+  rotor2Notch = "E"
+  rotor3 = "BDFHJLCPRTXVZNYEIWGAKMUSQO"
+  rotor3Notch = "V"
+  rotor4 = "ESOVPZJAYQUIRHXLNFTGKDCMWB"
+  rotor4Notch = "J"
+  rotor5 = "VZBRGITYUPSDNHLXAWMJQOFECK"
+  rotor5Notch = "Z"
 
-if inputs == 97:
-    inputs = 100
+  rotorDict = {"I":rotor1,"II":rotor2,"III":rotor3,"IV":rotor4,"V":rotor5}
+  rotorNotchDict = {"I":rotor1Notch,"II":rotor2Notch,"III":rotor3Notch,"IV":rotor4Notch,"V":rotor5Notch}  
 
-if inputs == 102:
-    inputs = 120
+  reflectorB = {"A":"Y","Y":"A","B":"R","R":"B","C":"U","U":"C","D":"H","H":"D","E":"Q","Q":"E","F":"S","S":"F","G":"L","L":"G","I":"P","P":"I","J":"X","X":"J","K":"N","N":"K","M":"O","O":"M","T":"Z","Z":"T","V":"W","W":"V"}
+  reflectorC = {"A":"F","F":"A","B":"V","V":"B","C":"P","P":"C","D":"J","J":"D","E":"I","I":"E","G":"O","O":"G","H":"Y","Y":"H","K":"R","R":"K","L":"Z","Z":"L","M":"X","X":"M","N":"W","W":"N","Q":"T","T":"Q","S":"U","U":"S"}
 
-if inputs == 103:
-    inputs = 122
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  rotorANotch = False
+  rotorBNotch = False
+  rotorCNotch = False
 
-if inputs == 98:
-    inputs = 110
+  if reflector=="UKW-B":
+    reflectorDict = reflectorB
+  else:
+    reflectorDict = reflectorC
 
-if inputs == 99:
-    inputs = 101
+  #A = Left,  B = Mid,  C=Right
+  rotorA = rotorDict[rotors[0]]
+  rotorB = rotorDict[rotors[1]]
+  rotorC = rotorDict[rotors[2]]
+  rotorANotch = rotorNotchDict[rotors[0]]
+  rotorBNotch = rotorNotchDict[rotors[1]]
+  rotorCNotch = rotorNotchDict[rotors[2]]
 
-
-#rotors
-def rotor(rotor):
-    rotor = inputs+1
-
-
-    rotor2 = rotor+1
-
-    rotor3 = rotor2+1
-
-    if rotor3 >= 122:
-        rotor3 = rotor3 -26
-
-    #reflection
-    reflect = rotor3 +1
-    reflect2 = reflect + 1
-    reflect3 = reflect2 +1
-
-    if reflect3 >= 122:
-        reflect3 = 97
-
-    print(reflect3)
-    print(chr(reflect3))
-
-
-rotor(rotor)
+  rotorALetter = ringPositions[0]
+  rotorBLetter = ringPositions[1]
+  rotorCLetter = ringPositions[2]
