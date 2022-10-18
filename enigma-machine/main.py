9df86949e8a00e5f9b943d26d7deae19aa58f45e
hauntedwaffledogs <evan1evan123@gmail.com>
2022-10-18 08:58:43 -0500

    working version

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -1,61 +1,23 @@
-# settings -----------------------------------
-rotors = ("I", "II", "III")
-reflector = "UKW-B"
-ringSettings = "ABC"
-ringPositions ="DEF"
-plugboard = "AT BS DE FM IR KN LZ OW PV XY"
-# --------------------------------------------
+from enigma.machine import EnigmaMachine
 
-def caesarShift(str, amount):
-    output=""
+# setup machine according to specs from a daily key sheet:
 
-    for i in range(0, len(str)):
-        c = str[i]
-        code = ord(c)
-        if ((code >= 65) and (code <=90)):
-            c = chr(((code -65 + amount) % 26) + 26)
-        output = output + c
+machine = EnigmaMachine.from_key_sheet(
+       rotors='II IV V',
+       reflector='B',
+       ring_settings=[1, 20, 11],
+       plugboard_settings='AV BS CG DL FU HZ IN KM OW RX')
 
-    return output
+# set machine initial starting position
+machine.set_display('WXC')
 
-def encode(plaintext):
-  global rotors, reflector,ringSettings,ringPositions,plugboard
-  #Enigma Rotors and reflectors
-  rotor1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
-  rotor1Notch = "Q"
-  rotor2 = "AJDKSIRUXBLHWTMCQGZNPYFVOE"
-  rotor2Notch = "E"
-  rotor3 = "BDFHJLCPRTXVZNYEIWGAKMUSQO"
-  rotor3Notch = "V"
-  rotor4 = "ESOVPZJAYQUIRHXLNFTGKDCMWB"
-  rotor4Notch = "J"
-  rotor5 = "VZBRGITYUPSDNHLXAWMJQOFECK"
-  rotor5Notch = "Z"
+# decrypt the message key
+msg_key = machine.process_text('KCH')
 
-  rotorDict = {"I":rotor1,"II":rotor2,"III":rotor3,"IV":rotor4,"V":rotor5}
-  rotorNotchDict = {"I":rotor1Notch,"II":rotor2Notch,"III":rotor3Notch,"IV":rotor4Notch,"V":rotor5Notch}  
+# decrypt the cipher text with the unencrypted message key
+machine.set_display(msg_key)
 
-  reflectorB = {"A":"Y","Y":"A","B":"R","R":"B","C":"U","U":"C","D":"H","H":"D","E":"Q","Q":"E","F":"S","S":"F","G":"L","L":"G","I":"P","P":"I","J":"X","X":"J","K":"N","N":"K","M":"O","O":"M","T":"Z","Z":"T","V":"W","W":"V"}
-  reflectorC = {"A":"F","F":"A","B":"V","V":"B","C":"P","P":"C","D":"J","J":"D","E":"I","I":"E","G":"O","O":"G","H":"Y","Y":"H","K":"R","R":"K","L":"Z","Z":"L","M":"X","X":"M","N":"W","W":"N","Q":"T","T":"Q","S":"U","U":"S"}
+ciphertext = 'AXKMVTCYBGVAV'
+plaintext = machine.process_text(ciphertext)
 
-  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  rotorANotch = False
-  rotorBNotch = False
-  rotorCNotch = False
-
-  if reflector=="UKW-B":
-    reflectorDict = reflectorB
-  else:
-    reflectorDict = reflectorC
-
-  #A = Left,  B = Mid,  C=Right
-  rotorA = rotorDict[rotors[0]]
-  rotorB = rotorDict[rotors[1]]
-  rotorC = rotorDict[rotors[2]]
-  rotorANotch = rotorNotchDict[rotors[0]]
-  rotorBNotch = rotorNotchDict[rotors[1]]
-  rotorCNotch = rotorNotchDict[rotors[2]]
-
-  rotorALetter = ringPositions[0]
-  rotorBLetter = ringPositions[1]
-  rotorCLetter = ringPositions[2]
+print(plaintext)
