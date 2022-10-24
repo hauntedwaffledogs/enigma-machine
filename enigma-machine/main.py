0a1e9437410cd74d5a10974508911b811fd360ba
Ness <64226529+GMan2006i@users.noreply.github.com>
2022-10-24 11:20:08 -0500

    Update main.py

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -1,8 +1,7 @@
-import numpy as np
-import string as stir
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 originalText = ""
-mainSeed = ""
+mainSeed = int()
 
 print("Input Plaintext")
 input(originalText)
@@ -12,3 +11,13 @@ input(mainSeed)
 
 # Encrypting Shit \/\/\/\/
 
+def caeser(PlTxt, Seed):
+    shiftAmount = (PlTxt + Seed)
+
+
+caeser(originalText, mainSeed)
+    
+while (shiftAmount > 26):
+    shiftAmount = shiftAmount - 26
+else:
+    returnText = 
