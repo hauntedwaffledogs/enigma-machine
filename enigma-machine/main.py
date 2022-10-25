3180f97aafdf4e35404c64f91079ad28f674fd26
Ness <64226529+GMan2006i@users.noreply.github.com>
2022-10-25 14:42:14 -0500

    Update main.py
    LETS FUCKING GO THE LIST SHIFTING WORKS

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -1,23 +1,36 @@
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+switchBack = int()
+# RE ADD THESE (anything with > ##) INTO CODE ONCE FUNCTION AND OTHER SHIT WORKS \/\/\/\/\/
 
-originalText = ""
-mainSeed = int()
+##print("Input Plaintext")
+##input(originalText)
 
-print("Input Plaintext")
-input(originalText)
-
-print("Input Seed, Use Numbers")
-input(mainSeed)
+##print("Input Seed, Use Numbers")
+##input(mainSeed)
 
 # Encrypting Shit \/\/\/\/
 
 def caeser(PlTxt, Seed):
-    shiftAmount = (PlTxt + Seed)
+    shiftAmount = Seed
+    return shiftAmount
 
-
-caeser(originalText, mainSeed)
+caeser(0, 0)
     
-while (shiftAmount > 26):
-    shiftAmount = shiftAmount - 26
-else:
-    returnText = 
+if shiftAmount > 26:
+    while shiftAmount > 26:
+        shiftAmount = shiftAmount - 26
+        #print(shiftAmount)
+        if shiftAmount in range(len(letters)):
+            break
+    
+for i in shiftedLetters:
+    if (shiftCounter + shiftAmount) >= 26:
+        switchBack = 26
+    else:
+        switchBack = 0
+    shiftedLetters[shiftCounter] = letters[(shiftCounter + (shiftAmount - switchBack))]
+    shiftCounter = shiftCounter + 1
+    print(shiftedLetters[(shiftCounter - 1)])
+    if shiftCounter == 26:
+        break
+    else:
+        continue
