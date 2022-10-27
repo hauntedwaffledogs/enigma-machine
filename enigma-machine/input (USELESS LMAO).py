e390ca26986feef170e7b577f07bdba5432b652f
Ness <gman2006i@gmail.com>
2022-10-26 19:43:03 -0500

    yes

--- enigma-machine/input (USELESS LMAO).py
+++ enigma-machine/input (USELESS LMAO).py
@@ -1,5 +1,17 @@
+encryptState = input('decrypt message, or encrypt message? (d/e): ')
+
+while encryptState != True or False:
+    if encryptState == 'd':
+       print('Sorry, this function is not avalible yet')
+       encryptState = input('decrypt message, or encrypt message? (d/e): ')
+    else:
+        if encryptState == 'e':
+            encryptState = True
+
 plTxt = input('Input your Message lowercase; letters only, no spaces, numbers or special characters: ')
-seed = input('Input your first Seed numbers (Use only numbers, no letters or Special characters): ')
+seed = input('Input your first Seed numbers; Use only numbers, no letters or Special characters: ')
+
+
 
 
 firstFile = open('transfer1.txt', 'w')
@@ -8,3 +20,8 @@ firstFile.write(plTxt)
 seed1File.write(seed)
 firstFile.close
 seed1File.close
+
+if encryptState == True:
+    print('To continue encryption, go to "string2numlist.py"')
+else:
+    print("YOU SHOULD'NT BE HERE YOU HERATIC")
