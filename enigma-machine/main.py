729808bd5dd9890218e49632ce6bfe6dbfcbd7d0
hauntedwaffledogs <evan1evan123@gmail.com>
2022-11-03 08:01:15 -0700

    FARTZ

--- enigma-machine/main.py
+++ enigma-machine/main.py
@@ -1,2 +1,12 @@
-x = 16
-
+import random
+fart = random.randrange(1, 11)
+inputs = input('message to encrypt: ')
+for r in inputs:
+    l = ord(r)
+    g = l + fart
+    op = chr(g)
+    print(op)
+fart = str(fart)
+file1 = open('key1.txt', 'w')
+file1.write(fart)
+file1.close
